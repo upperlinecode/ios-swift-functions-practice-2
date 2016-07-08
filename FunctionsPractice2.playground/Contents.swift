04a625ab579973b05068f1b8dd341c3e3805064a
carsonlevine <carsonglevine@me.com>
2016-07-08 15:22:16 -0400

    first commit

---
+++ FunctionsPractice2.playground/Contents.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+//Functions with parameters
+
+//Make a function called double that takes one integer parameter. Make it return twice the value of the parameter.
+//double(3) --> returns 6
+//double(20) --> returns 40
+
+
+
+
+
+
+
+//Make a function called repeat that takes in a string parameter and prints that string three times. The first two should be followed by a comma, and the final should be followed by an exclamation point.
+//repeat("Banana") --> returns "Banana, Banana, Banana!"
+//repeat("That kangaroo is Australian") --> returns "That kangaroo is Australian, That kangaroo is Australian, That kangaroo is Australian!"
+
+
+
+
+
+
+//Make a function called pricePlusTax that calculates the price of a product plus 7% sales tax. It should accept a parameter of type Double
+//pricePlusTax(1.00) --> returns 1.07
+//pricePlusTax(7.15) --> returns 7.65
+
+
+
+
+
+
+
+//A person's max heart rate is approximately 220 minus their age. Define a function called maxHeartRate that takes in a parameter for the age of a person and returns the maximum heart rate for that person.
+//maxHeartRate(30) --> 190
+//maxHeartRate(17) --> 203
+
+
+
+
+
+
+//Make a function called todaysActivities that takes in a string parameter called weather. If it is raining, the function should return 'Staying inside and reading a book.'. If it is sunny, it will return 'Going swimming!'. If it is windy, it will return 'Flying a kite!' If weather contains any other string, it will return 'I don't know, I'll probably play it by ear.'
+//todaysActivities("windy") --> "Flying a kite."
+//todaysActivities("temperate") --> "I don't know, I'll probably play it by ear."
+
+
+
+
+
+
+
+
+
+
+//Make a function called doesThisStartWithQ that accepts a string as a parameter and returns true if the first letter of the string is 'q' or 'Q' and returns false otherwise.
+//doesThisStartWithQ("Bottle Cap") --> false
+//doesThisStartWithQ("Quincy") --> true
+
+
+
+
+
+
+
+
+
+
+
+
+//Bonus
+//More than one parameter
+//Make a function called pythagoreanTheorem that takes in two integer parameters for leg1 and leg2 of a right triangle. The function should return the length of the hypotenuse.
+//pythagoreanTheorem(3,4) --> 5
+//pythagoreanTheorem(6,8) --> 10
